2a7580462ad337e5e764109114268f530755fb51
Unknown <simon.frederik@web.de>
2019-04-25 14:11:50 +0200

    Had to adjust allowed hosts

--- myshelf/settings.py
+++ myshelf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'rp2%wk(79!z_poj_v=4$ld83@fadqi
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['floating-brook-45418.herokuapp.com']
+ALLOWED_HOSTS = ['myshelfapp.herokuapp.com']
 
 
 # Application definition
